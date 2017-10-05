cdf625b7f763ff50b422e28e7f4ab97e29e02f36
Christoph <Christoph@CHRISTOPH-VAIO>
2017-10-05 19:01:53 +0200

    Change SVA 2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            cboInsurance.ItemsSource = new List<string>() { "SVA", "WGKK", "NÖGKK", "BVA", "BGGK", "SGGK" };
+            cboInsurance.ItemsSource = new List<string>() { "SVA", "WGKK", "NÖGKK", "BVA", "BGGK", "SGGK", "PVA" };
             cboInsurance.SelectedIndex = 0;
         }
 
